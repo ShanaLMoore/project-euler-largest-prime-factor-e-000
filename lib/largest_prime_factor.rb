34a14e6775752f8e5a8e9e1ccabc5917d628e47c
Shana L Moore <shana.lavina.moore@gmail.com>
2015-10-20 16:05:06 -0700

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,12 @@
 # Enter your procedural solution here!
+#A prime number can only be divided by 1 or itself, so it cannot be factored any further! 
+
+require 'prime'
+
+def largest_prime_factor(number)
+  result = number.prime_division 
+  result.last[0]
+end
+
+ 
+
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,15 @@
 # Enter your object-oriented solution here!
+require 'prime'
+
+class LargestPrimeFactor
+
+  def initialize (input)
+    @input = input
+  end 
+
+  def number
+    result = @input.prime_division 
+    result.last[0]
+  end
+
+end
